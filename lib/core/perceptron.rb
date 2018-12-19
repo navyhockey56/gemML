d40f383f0ad13a0b07d439823fa284e08314b886
Will Dengler <wdengler@jibe.com>
2018-12-18 23:15:32 -0500

    Working out bugs in perceptron

--- lib/core/perceptron.rb
+++ lib/core/perceptron.rb
@@ -11,31 +11,36 @@ module GemML
     end
 
     def evaluate(data:,threshold:0,default:1)
-      activation = GemML::Tools.dot_product(@weights, data[:data]) + @bias
-      return 0 if activation == threshold
-      activation > threshold ? 1 : -1
+      activation = GemML::Tools.dot_product(@weights, data[:features]) + @bias
+      label = 0 if activation == threshold
+      label ||= activation > threshold ? 1 : -1
+      {:label => label, :activation => activation}
     end
 
-    def Perceptron.train(data:data, max_iterations:10)
+    def Perceptron.train(data:, max_iterations:100)
       # Initialize weights and bias to 0
-      weights = (0..data.first[:data].count).map {|i| 0}
+      weights = (0...data.first[:features].count).map {|i| 0}
       bias = 0
       
       # Proceed for the maximum iterations, or until the
       # perception converges
-      (0..max_iterations).each do |i|
-
+      (0...max_iterations).each do |i|
+        puts "Beginning iteration #{i + 1}"
         # Randomizing the data each iteration speeds up convergence
-        randomized_data = data.suffle
+        randomized_data = data.shuffle
 
         has_converged = true
         randomized_data.each do |example|
+          
           # Calculate the activation for the point actvation = dot(w,d) + b
-          activation = GemML::Tools.dot_product(weights, example[:data]) + bias
-          if activation <= 0
+          activation = GemML::Tools.dot_product(weights, example[:features]) + bias
+          puts "Activation #{activation}, #{example}"
+          if example[:label] * activation <= 0
             # The perceptron incorrectly labeled the data,
             # adjust the weights and bias
-            weights.map! {|w| w + example[:label] * example[:data]}
+            weights = (0...weights.count).map { |i|
+               weights[i] + example[:label] * example[:features][i]
+            }
             bias += example[:label]
             # Incorrect labeling => not yet converged
             has_converged = false
@@ -44,6 +49,7 @@ module GemML
 
         # The training data was perfectly labeled, no more update 
         # will be made, create a new perception
+        puts "The percepetron converged in #{i+1} iterations"
         return new(weights:weights, bias:bias) if has_converged
       end
 
